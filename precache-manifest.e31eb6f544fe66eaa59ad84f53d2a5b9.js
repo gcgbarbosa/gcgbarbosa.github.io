d9176671d981c7978eb9cf24c849c2f1e9109f79
gcgbarbosa <gcgbarbosa@gmail.com>
2020-06-10 15:32:34 -0700

    Updates

---
+++ precache-manifest.e31eb6f544fe66eaa59ad84f53d2a5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fe91803feceb0f1225df195e294a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e46b7cef7ded8080140",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7254849ae757387ccace",
+    "url": "/static/js/2.93686b8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.93686b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e46b7cef7ded8080140",
+    "url": "/static/js/main.ba5fb25c.chunk.js"
+  },
+  {
+    "revision": "5f02c238ad28db695265",
+    "url": "/static/js/runtime-main.f794b581.js"
+  }
+]);
